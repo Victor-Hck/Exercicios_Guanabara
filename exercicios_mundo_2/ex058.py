1c47fdf333466636ccc1959829b577767b58f479
Exercicios_Guanabara <victor.profissional3592@gmail.com>
2025-01-21 09:37:42 -0300

    update

---
+++ exercicios_mundo_2/ex058.py
@@ -0,0 +1,7 @@
+"""
+Desafio 058
+
+Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um
+Número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
+Palpites foram necessários para vencer.
+"""
---
+++ exercicios_mundo_2/ex061.py
@@ -0,0 +1,6 @@
+"""
+Desafio 061
+
+Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA
+Mostrando os 10 primeiros termos da progressão usando a estrutura while.
+"""
