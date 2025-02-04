d9a39c6583f1f4168ac42ba98cdf61fdc68cd98a
Exercicios_Guanabara <victor.profissional3592@gmail.com>
2025-02-04 18:23:32 -0300

    update

--- exercicios_mundo_3/ex072.py
+++ exercicios_mundo_3/ex072.py
@@ -7,3 +7,10 @@ Com uma contagem por extenso, de zero até vinte.
 Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 e mostrá-lo por extenso.
 """
+numeros_extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze","doze", "treze", "quatorze", "quinze", "dezesseis","dezessete", "dezoito", "dezenove", "vinte")
+while True:
+    number = int(input("Digite um número entre 0 e 20: "))
+    if 0 <= number <= 20:
+        break
+    print("Tente novamente. ", end='')
+print(f"Você digitou o número {numeros_extenso[number]}")
--- exercicios_mundo_3/ex073.py
+++ exercicios_mundo_3/ex073.py
@@ -8,3 +8,9 @@ B) Os últimos 4 colocados da tabela.
 C) Uma lista com os times em ordem alfabética.
 D) Em que posição na tabela está o time da Chapecoense.
 """
+tabela_brasileirao = ("Palmeiras", "Flamengo", "Corinthians", "Atlético-MG", "Grêmio", "Athletico-PR", "Internacional", "Fluminense", "Botafogo", "São Paulo", "Cuiabá", "Fortaleza", "Cruzeiro", "Vasco", "Bahia", "Goiás", "Santos", "Coritiba", "América-MG", "Avaí")
+print(f"")
+print(f"Os 5 primeiros são {tabela_brasileirao[0:5]}")
+print(f"Os 4 últimos são {tabela_brasileirao[-4:]}")
+print(f"Times em ordem alfabética: {sorted(tabela_brasileirao)}")
+print(f"O Atlético-MG está na {tabela_brasileirao.index('Atlético-MG')+1} posição")
