ebb31f4616123b36b51d1dd0cd16b5e1f7f20734
Exercicios_Guanabara <victor.profissional3592@gmail.com>
2025-02-12 17:09:44 -0300

    update

---
+++ exercicios_mundo_3/ex085.py
@@ -0,0 +1,7 @@
+"""
+Desafio 085
+
+Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os
+Em uma lista única que mantanha separados os valores pares e ímpares. No final, mostre os valores
+Pares e ímpares em ordem crescente.
+"""
