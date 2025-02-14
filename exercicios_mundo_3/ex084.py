0b25cb407dfa1d465e30b6f12a24df7fb3ca053d
Exercicios_Guanabara <victor.profissional3592@gmail.com>
2025-02-14 18:13:37 -0300

    update

--- exercicios_mundo_3/ex084.py
+++ exercicios_mundo_3/ex084.py
@@ -8,3 +8,36 @@ A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.
 """
+temp = list() # Lista temporaria
+princ = list() # Lista principal
+maior = menor = 0
+
+while True:
+    temp.append(input("Nome: "))
+    temp.append(float(input("Peso:")))
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = input("Deseja continuar? [S/N]").upper()
+    if resp == "N":
+        break
+
+
+print(f"Os dados foram {princ}")
+print(f"Ao todo você cadastrou {len(princ)}")
+print(f"O maior peso foi de {maior}Kg. Peso de", end='')
+for p in princ:
+    if p[1] == maior:
+        print(f"[{p[0]}] ", end='')
+print()
+print(f"O menor peso foi de {menor}Kg. Peso de ", end='')
+for p in princ:
+    if p[1] == menor:
+        print(f"[{p[0]}] ", end='')
+print()
--- exercicios_mundo_3/ex085.py
+++ exercicios_mundo_3/ex085.py
@@ -5,3 +5,21 @@ Crie um programa onde o usuário possa digitar sete valores numéricos e cadastr
 Em uma lista única que mantanha separados os valores pares e ímpares. No final, mostre os valores
 Pares e ímpares em ordem crescente.
 """
+treino = [[], []]
+valor = 0
+
+for i in range(1, 8):
+    valor = int(input(f"Digite o {i} valor: "))
+
+    if valor % 2 == 0:
+        treino[0].append(valor)
+    else:
+        treino[1].append(valor)
+
+treino[0].sort()
+treino[1].sort()
+print("-=" * 30)
+print(f" Os valores pares digitados forem: {treino[0]}")
+print(f" Os valores impares digitados forem: {treino[1]}")
+
+
